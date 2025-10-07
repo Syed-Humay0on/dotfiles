bfc2659d4d15576ea6204eff40d4fe6d91fd9561
Proto <Humayoon04syed@gmail.com>
2025-10-07 20:10:32 +0500

    new changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,6 +3,9 @@
 # Terminate existing bar instances
 polybar-msg cmd quit
 
+export DISPLAY=:0
+polybar-msg cmd restart
+
 # Launch bar
 echo "---" | tee -a /tmp/polybar1.log 
 polybar Nordbar 2>&1 | tee -a /tmp/polybar1.log & disown
